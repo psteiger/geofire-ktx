2f638895afb1a74d94d648940d4520a9f9c42445
Patrick Pamponet Steiger <psteiger@gmail.com>
2021-08-22 02:56:52 -0300

    Use Kotlin's own `trySend` method instead of our own `tryOffer`.

--- geofire/src/main/java/com/freelapp/geofire/util/Util.kt
+++ geofire/src/main/java/com/freelapp/geofire/util/Util.kt
@@ -6,9 +6,6 @@ import kotlinx.coroutines.channels.SendChannel
 
 internal typealias Key = String
 
-internal fun <E> SendChannel<E>.tryOffer(element: E): Boolean =
-    runCatching { offer(element) }.getOrDefault(false)
-
 @PublishedApi
 internal inline fun <reified T : Any> DataSnapshot.getTypedValue(): T? =
     getValue(object : GenericTypeIndicator<T>() {})
